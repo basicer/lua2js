f9bf7ab200888308c5ccffb53e280238c7f7b36e
basicer <basicer@basicer.com>
2014-05-06 23:18:56 -0400

    Changes

--- test.js
+++ test.js
@@ -1,21 +1,23 @@
 var peg = require("pegjs");
 var gen = require("escodegen");
 var fs = require("fs");
+var vm = require("vm");
+
 
 var lang = fs.readFileSync("lua.pegjs").toString();
 var parser = peg.buildParser(lang);
+var exec = require('child_process').exec;
 
 function leval(src) {
 	var AST;
 	try { 
 	    AST = parser.parse(src);
 	} catch ( e ) {
+		console.log(e);
 	    return e.toString();
 	}
 
-	var code = gen.generate(AST);
-	console.log(code);
-	return new Function(code);
+	return function(code) { return function() { return "(function() " + code + ")()"; } }(gen.generate(AST));
 }
 
 tests = [
@@ -57,22 +59,67 @@ tests = [
 
 ];
 
+function makenv() {
+
+	return (function(stdout) {
+		var env = {};
+		env.print = function() {
+			var s = Array.prototype.slice.call(arguments).join("\t"); 
+			stdout = stdout + s + "\n";
+			console.log(s); 
+		};
+
+		env.getStdOut = function() { return stdout; }
+		
+		return env;
+	})("");
+
+}
 
 var idx = 0;
 tests.forEach(function(nfo) {
 	var code = nfo[0];
-	var result = nfo[1];
-	exports["test" + idx++] = function(test) {
+	var result = nfo[1]
+	exports["testSimple" + idx++] = function(test) {
 		test.expect(2);
 
 		if ( typeof(code) != "string" ) code = code.join("\n");
 
 		var v = leval(code);
 		test.ok(typeof(v) == "function", v);
-		if ( typeof(v) == "function" ) test.equal(v(), result);
+		if ( typeof(v) == "function" ) {
+			var env = makenv();
+			var a = vm.runInNewContext(v(), makenv(), "vm");
+			test.equal(a, result);
+		}
 		else test.ok(false, "Coudnt run");
 
 		test.done();
 	}
 
 });
+
+
+fs.readdirSync("./lua-tests").forEach(function(f) {
+	exports["testLua" + f] = function(test) {
+		var code = fs.readFileSync("./lua-tests/" + f).toString();
+		test.expect(2);
+
+		var v = leval(code);
+		test.ok(typeof(v) == "function", v);
+		if ( typeof(v) == "function" ) {
+			(function(env) {
+				var a = vm.runInNewContext(v(), env, "vm");		
+
+				exec('lua ./lua-tests/' + f, function(err, stdout, stderr) {
+					if ( stderr !== "" ) test.ok(false, stderr);
+					test.equals(env.getStdOut(), stdout );
+					test.done();
+				});
+			})(makenv());
+		}
+		else test.ok(false, "Coudnt parse.");
+	};
+
+});
+
